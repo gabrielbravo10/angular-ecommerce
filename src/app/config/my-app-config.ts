dac3b8bd90a5bcd3d0e08899fd6b268503ffd9fc
Gabriel Bravo <>
2021-01-05 07:36:59 -0400

    okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa3b3pxl4Z5iRLoZ5d6',
+        issuer: 'https://dev-8730321.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
